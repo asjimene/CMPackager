449cc407e439100f5df1fa9eeea599e75edbfcad
Andrew Jimenez <asjimenez91@gmail.com>
2018-04-03 09:27:30 -0700

    Update Google Chrome Recipe

--- ExtraFiles/GoogleChrome/installChrome.ps1
+++
@@ -1,8 +0,0 @@
-$OSArch = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
-if ($OSArch -eq "64-bit"){
-    $args = "/i googlechromestandaloneenterprise64.msi","/q"
-    Start-Process msiexec.exe -ArgumentList $args -Wait
-} else {
-    $args = "/i googlechromestandaloneenterprise.msi","/q"
-    Start-Process msiexec.exe -ArgumentList $args -Wait
-}
